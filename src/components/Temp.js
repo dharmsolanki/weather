64f33244412ba8ac68519c8d441379cfd20828fe
dharmsolanki <dharmsolanki1010@gmail.com>
2023-11-08 17:24:31 +0530

    enhence design

--- src/components/Temp.js
+++ src/components/Temp.js
@@ -6,19 +6,21 @@ export default function Temp() {
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  
+  const [click, setClick] = useState(false);
+
   useEffect(() => {
     const fetchWeatherData = async () => {
       try {
         setLoading(true);
         setError(null);
-        
+  
         const url = `https://api.weatherapi.com/v1/current.json?key=16b7a01ea69d4ac8bb160343230811&q=${searchText}&aqi=yes`;
         const response = await fetch(url);
         const data = await response.json();
-
+  
         if (response.ok) {
           setWeatherData(data);
+          setClick(true); // Set click to true when data is fetched
         } else {
           setError("Failed to fetch weather data");
         }
@@ -38,22 +40,63 @@ export default function Temp() {
   };
 
   return (
-    <>
-      <div className="box">
-        <input type="search" className="search" onChange={handleSearch} />
-        {loading && <p>Loading...</p>}
-        {error && !loading && (
-          <p className="error-message">{error}</p>
-        )}
-        {weatherData && !loading && !error && (
-          <div className="info">
-            <h2>City Name: {searchText}</h2>
-            <p>Temperature: {weatherData.current.temp_c}°C</p>
-            <p>Weather: {weatherData.current.condition.text}</p>
-            {/* Add more data you want to display here */}
+    <div className="container weather-container my-5">
+      <div className="card horizontal-card p-4">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src="images/logo.png"
+              alt="Weather Icon"
+              className="img-fluid"
+            />
           </div>
-        )}
+          <div className="col-md-8">
+            <h1 className="text-center">Weather App</h1>
+            <div className="input-group mb-3">
+              <input
+                type="search"
+                className="form-control"
+                placeholder="Enter a city name"
+                onChange={handleSearch}
+              />
+              <div className="input-group-append">
+                <button
+                  className="btn btn-primary"
+                  type="button"
+                  // onClick={fetchWeatherData} // Call the function here
+                >
+                  <img src="images/search-icon.png" alt="Search" height="20px" width="20px" />
+                </button>
+              </div>
+            </div>
+            {loading && <p className="loading text-center">Loading...</p>}
+            {error && !loading && (
+              <div className="alert alert-danger my-3">{error}</div>
+            )}
+            {weatherData && !loading && !error && click && (
+              <div className="my-3">
+                <h2>Weather Information</h2>
+                <table className="table">
+                  <tbody>
+                    <tr>
+                      <th>City Name</th>
+                      <td>{weatherData.location.name}</td>
+                    </tr>
+                    <tr>
+                      <th>Temperature</th>
+                      <td>{weatherData.current.temp_c}°C</td>
+                    </tr>
+                    <tr>
+                      <th>Weather</th>
+                      <td>{weatherData.current.condition.text}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
